2d2a779af93629c758acd525ba0221fa349db81f
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-12-02 16:22:56 +0100

    Showing dummy meetups

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const Home = () => {
+    const DUMMY_MEETUPS = [
+        {
+            id: 'm1',
+            title: '1 meetup',
+            image: 'https://picsum.photos/id/237/1920/480',
+            address: 'Some address 5, 12345 Some City',
+            description: 'This is a 1 meetup!'
+        },
+        {
+            id: 'm2',
+            title: '2 meetup',
+            image: 'https://picsum.photos/id/238/1920/480',
+            address: 'Some address 7, 12345 Some City',
+            description: 'This is a 2 meetup!'
+        },
+        {
+            id: 'm3',
+            title: '3 meetup',
+            image: 'https://picsum.photos/id/239/1920/480',
+            address: 'Some address 5, 12345 Some City',
+            description: 'This is a 3 meetup!'
+        }
+    ];
+
+    return (
+        <MeetupList meetups={DUMMY_MEETUPS} />
+    )
+}
+
+export default Home;
