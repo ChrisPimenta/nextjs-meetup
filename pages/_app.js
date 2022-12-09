c4412f43ac5d24fae1339123172f22e25c9bb5b5
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-12-09 14:55:39 +0100

    Wrap Strict Mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout'
+import React from 'react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <React.StrictMode>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </React.StrictMode>
   )
 }
 
